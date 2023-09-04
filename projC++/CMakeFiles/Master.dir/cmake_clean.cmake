0623b5cb02737e1151bb8cabfd2b13278026a371
dar-sn <131444982+dar-sn@users.noreply.github.com>
2023-09-04 16:20:08 +0500

    Add files via upload

---
+++ projC++/CMakeFiles/Master.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Master.dir/src/main.cpp.o"
+  "Master"
+  "Master.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Master.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- projC++/src/main.cpp
+++ projC++/src/main.cpp
@@ -2,4 +2,29 @@
 
 int main() {
     std::cout << "Hello Easy C++ project!" << std::endl;
+    int num;
+    std:: cin >> num;
+    int num2;
+    std:: cin >> num2;
+    int num3 = num + num2;
+    std:: cout << num3 << " = your sum" << std::endl;
+    if (num3 < 10)
+    {
+      std:: cout << num3 << " = your sum" << std::endl;
+    }
+    else
+    {
+        std:: cout << "sum bigger 10" << std:: endl;
+
+    }
+    /*switch (num)
+    {
+    case 1:
+        code 
+        break;
+    
+    default:
+        break;
+    }*/
+    return 0;
 }
